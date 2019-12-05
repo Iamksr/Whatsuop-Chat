f922ba13952f0fc93be2782f06043f4287e31989
Arvind Kushwah <arvindkushwah9@gmail.com>
2019-12-05 19:11:58 +0530

    fix issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 	    protected   
     def configure_permitted_parameters  
       devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-        user_params.permit({ roles: [] }, :email, :password, :password_confirmation, :username, :image)
+        user_params.permit({ roles: [] }, :email, :password, :password_confirmation, :username)
     end
       devise_parameter_sanitizer.permit(:account_update) do |user_params|
       user_params.permit({ roles: [] }, :email, :password, :password_confirmation, :username, :image)
