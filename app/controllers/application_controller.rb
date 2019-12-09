9dc889e2e34283a377974c1f4b7e14633bfe305e
Arvind Kushwah <arvindkushwah9@gmail.com>
2019-12-09 19:21:40 +0530

    master

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,27 @@
 class ApplicationController < ActionController::Base
-	  before_action :authenticate_user!
-	   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  before_action :authenticate_user!
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
 
 
-	    protected   
-    def configure_permitted_parameters  
-      devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-        user_params.permit({ roles: [] }, :email, :password, :password_confirmation, :username, :image)
-    end
-      devise_parameter_sanitizer.permit(:account_update) do |user_params|
-      user_params.permit({ roles: [] }, :email, :password, :password_confirmation, :username, :image)
-    end
+
+  def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+
+  user_params.permit({ roles: [] }, :email, :password, :password_confirmation,:username,:image)
+
   end
-  
+
+
+
+  devise_parameter_sanitizer.permit(:account_update) do |user_params|
+
+  user_params.permit({ roles: [] }, :email, :password, :password_confirmation,:username,:image)
+
+  end
+  end
+
 end
